df372be0e89982035e7130e2ac1d2f8bfbd3780c
MarioMunera <samuelmariomunera@gmail.com>
2025-10-02 18:30:54 -0500

    Reorganizar el index

--- src/scripts/hamburgerMenu.js
+++ src/scripts/hamburgerMenu.js
@@ -1,7 +1,6 @@
 const toggleButton = document.getElementById('button-menu');
 const mainNav = document.querySelector('.main-nav');
 
-
 toggleButton.addEventListener('click', () => {
   toggleButton.classList.toggle('close');
   mainNav.classList.toggle('show');
